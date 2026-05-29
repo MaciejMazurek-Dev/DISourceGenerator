0f6cedab306d3c9250a901b986f9c409b5d358fa
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2026-05-29 20:35:53 +0200

    Add project files.

---
+++ src/DISourceGenerator.Library/DISourceGenerator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace DISourceGenerator.Library
+{
+    [Generator]
+    public class DISourceGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext initContext)
+        {
+        }
+    }
+}
