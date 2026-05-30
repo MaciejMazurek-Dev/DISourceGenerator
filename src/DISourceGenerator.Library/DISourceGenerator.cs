8a57bcb99cc832fb36c9af7e58ac98d8e54e94a0
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2026-05-30 19:30:37 +0200

    Add marker attribute

--- src/DISourceGenerator.Library/DISourceGenerator.cs
+++ src/DISourceGenerator.Library/DISourceGenerator.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.CodeAnalysis;
 
-namespace DISourceGenerator.Library
+namespace DISourceGenerator.Library;
+
+[Generator(LanguageNames.CSharp)]
+public class DISourceGenerator : IIncrementalGenerator
 {
-    [Generator]
-    public class DISourceGenerator : IIncrementalGenerator
+    public void Initialize(IncrementalGeneratorInitializationContext initContext)
     {
-        public void Initialize(IncrementalGeneratorInitializationContext initContext)
+        // Add marker attribute 
+        initContext.RegisterPostInitializationOutput(callback =>
         {
-        }
+            const string markerAttribute = @"
+                                                [AttributeUsage(AttributeTargets.Class)]
+                                                public class MarkerAttribute : Attribute
+                                                {
+                                                }
+                                            ";
+            callback.AddSource("MarkerAttribute.g.cs", markerAttribute);
+        });
     }
 }
