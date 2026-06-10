310cfb5e283c85ffeb88769b3a08bdce9a1c529b
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2026-06-10 16:34:29 +0200

    Add snapshot test

---
+++ tests/DISourceGenerator.Tests/GeneratorSnapshotTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using VerifyXunit;
+
+namespace DISourceGenerator.Tests
+{
+    public class GeneratorSnapshotTests
+    {
+        [Fact]
+        public Task GeneratesSourceCodeCorrectly()
+        {
+            string source = @"
+                              [Marker]
+                              public class partial TestClass
+                              {
+                              }
+                              ";
+
+            return TestHelper.Verify(source);
+        }
+    }
+}
---
+++ tests/DISourceGenerator.Tests/TestHelper.cs
@@ -0,0 +1,30 @@
+﻿using DISourceGenerator.Library;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace DISourceGenerator.Tests
+{
+    public static class TestHelper
+    {
+        public static Task Verify(string sourceCode)
+        {
+            // Parse source code into syntax tree
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
+
+            // Compile syntax tree
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName: "SourceGeneratorTest", 
+                syntaxTrees: new[] { syntaxTree });
+
+            // Create instance of source generator
+            SourceGenerator generator = new();
+
+            // Run generator over compiled syntax tree
+            GeneratorDriver generatorDriver = CSharpGeneratorDriver.Create(generator);
+            generatorDriver = generatorDriver.RunGenerators(compilation);
+
+            // Run snapshot test on source generator output
+            return Verifier.Verify(generatorDriver);
+        }
+    }
+}
