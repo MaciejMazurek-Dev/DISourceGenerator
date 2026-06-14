c6d907f58dd4741422aef8dcf5f2dad40e338c6d
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2026-06-14 14:35:30 +0200

    Fix a bug with missing references to the assembly

--- tests/DISourceGenerator.Tests/TestHelper.cs
+++ tests/DISourceGenerator.Tests/TestHelper.cs
@@ -1,20 +1,43 @@
 ﻿using DISourceGenerator.Library;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
+[assembly: CaptureConsole]
 namespace DISourceGenerator.Tests
 {
+
     public static class TestHelper
     {
+        [ModuleInitializer]
+        public static void Init() => VerifySourceGenerators.Initialize();
+
+
         public static Task Verify(string sourceCode)
         {
             // Parse source code into syntax tree
             SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(sourceCode);
 
+            // Get reference to .NET runtime library 
+            IEnumerable <PortableExecutableReference> references = new[]
+                {
+                    MetadataReference.CreateFromFile(typeof(System.Object).Assembly.Location),
+                };
+
             // Compile syntax tree
             CSharpCompilation compilation = CSharpCompilation.Create(
                 assemblyName: "SourceGeneratorTest", 
-                syntaxTrees: new[] { syntaxTree });
+                syntaxTrees: new[] { syntaxTree },
+                references: references);
+
+            // Display compilation errors
+            ImmutableArray<Diagnostic> diagnostic = compilation.GetDiagnostics();
+            foreach(var diag in diagnostic)
+            {
+                Console.WriteLine("Compilation error: " + diag.GetMessage());
+            }
 
             // Create instance of source generator
             SourceGenerator generator = new();
